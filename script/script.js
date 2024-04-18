98462876f885363082da28c254a1321464e4d282
mariangelamasci <162448399+mariangelamasci@users.noreply.github.com>
2024-04-18 11:24:08 +0200

    Update script.js

--- script/script.js
+++ script/script.js
@@ -1,1 +1,7 @@
-
+function doLong() {
+  var username = document.getElementByld("username").value;
+  var password = document.getElementByld("password").value;
+  if (username == "" || password == "") {
+    alert (" Username e password sono obbligatori");
+  }
+} 
