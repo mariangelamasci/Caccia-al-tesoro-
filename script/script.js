2aaa35b269338c81bc59c2b396306f8764968a03
mariangelamasci <162448399+mariangelamasci@users.noreply.github.com>
2024-04-30 18:24:24 +0200

    Update script.js

--- script/script.js
+++ script/script.js
@@ -4,4 +4,21 @@ function doLogin() {
   if (username == "" || password == "") {
     alert (" Username e password sono obbligatori");
   }
-} 
+els {
+//chiama il server per effetuare la verifica 
+fetch ( "https://www.cacciaapi.altervista.org/login.php/loginIpad', {
+method: "POST', // o 'GET'
+headers: {
+'Content-Type': 'application/json',
+},
+body: JSON. stringify({
+"username": username,
+"password": password
+})
+})
+-then(response => response.json()) // converte la risposta in JSON
+-then (data →> console.log(data)) // stampa i dati
+.catch((error) => {
+console.error ('Errore:', error);
+});
+}
